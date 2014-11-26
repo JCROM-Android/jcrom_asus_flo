a034ed5ead6616773cda055fccc218a975a57b79
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 21:51:32 +0900

    Updated for Lollipop

--- device-flo.mk
+++ device-flo.mk
@@ -9,26 +9,64 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.ui.phablet=286
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
-	$(LOCAL_PATH)/proprietary/system/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd \
-	$(LOCAL_PATH)/proprietary/system/etc/apns-conf.xml:system/etc/apns-conf.xml \
-	$(LOCAL_PATH)/proprietary/system/etc/audio_effects.conf:system/etc/audio_effects.conf \
-	$(LOCAL_PATH)/proprietary/system/etc/fmas_eq.dat:system/etc/fmas_eq.dat \
-	$(LOCAL_PATH)/proprietary/system/lib/libDxHdcp.so:system/lib/libDxHdcp.so \
-	$(LOCAL_PATH)/proprietary/system/lib/hw/keystore.msm8960.so:system/lib/hw/keystore.msm8960.so \
-	$(LOCAL_PATH)/proprietary/system/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libgcastv2_base.so:system/lib/libgcastv2_base.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libgcastv2_support.so:system/lib/libgcastv2_support.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libjgcastservice.so:system/lib/libjgcastservice.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libjhead.so:system/lib/libjhead.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libjhead_jni.so:system/lib/libjhead_jni.so
+	$(LOCAL_PATH)/proprietary/system/etc/preferred-apps/google.xml:system/etc/preferred-apps/google.xml:asus \
+	$(LOCAL_PATH)/proprietary/system/etc/sysconfig/google.xml:system/etc/sysconfig/google.xml:asus \
+	$(LOCAL_PATH)/proprietary/system/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml:asus \
+	$(LOCAL_PATH)/proprietary/system/etc/updatecmds/google_generic_update.txt:system/etc/updatecmds/google_generic_update.txt:asus \
+	$(LOCAL_PATH)/proprietary/system/etc/apns-conf.xml:system/etc/apns-conf.xml:asus \
+	$(LOCAL_PATH)/proprietary/system/etc/fmas_eq.dat:system/etc/fmas_eq.dat:asus \
+	$(LOCAL_PATH)/proprietary/system/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so:asus \
+	$(LOCAL_PATH)/proprietary/system/lib/libDxHdcp.so:system/lib/libDxHdcp.so:asus \
+	$(LOCAL_PATH)/proprietary/system/lib/librefocus.so:system/lib/librefocus.so:asus \
+	$(LOCAL_PATH)/proprietary/system/lib/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so:asus \
+	$(LOCAL_PATH)/proprietary/system/lib/libvorbisencoder.so:system/lib/libvorbisencoder.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libWVphoneAPI.so:system/vendor/lib/libWVphoneAPI.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd:asus \
+	$(LOCAL_PATH)/proprietary/system/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/NFCFailure.ogg:system/media/audio/ui/NFCFailure.ogg:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/NFCInitiated.ogg:system/media/audio/ui/NFCInitiated.ogg:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/NFCSuccess.ogg:system/media/audio/ui/NFCSuccess.ogg:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/NFCTransferComplete.ogg:system/media/audio/ui/NFCTransferComplete.ogg:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/NFCTransferInitiated.ogg:system/media/audio/ui/NFCTransferInitiated.ogg:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/audio_end.ogg:system/media/audio/ui/audio_end.ogg:asus \
+	$(LOCAL_PATH)/proprietary/system/media/audio/ui/audio_initiate.ogg:system/media/audio/ui/audio_initiate.ogg:asus
 
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage11.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carrier=wifi-only \
+    ro.config.ringtone=Girtab.ogg \
+    ro.config.notification_sound=Tejat.ogg \
+    ro.config.alarm_alert=Oxygen.ogg \
+    fmas.spkr_6ch=35,20,110 \
+    fmas.spkr_2ch=35,25 \
+    fmas.spkr_angles=10 \
+    fmas.spkr_sgain=0 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    keyguard.no_require_sim=true \
+    ro.facelock.black_timeout=1250 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=1000 \
+    ro.facelock.use_intro_anim=true \
+    ro.media.effect.bgdropper.adj=0.2 \
+    camera.flash_off=0 \
+    ro.com.widevine.cachesize=16777216 \
+    ro.setupwizard.network_required=true
+
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
 
--- proprietary/extract-files.sh
+++ proprietary/extract-files.sh
@@ -1,48 +1,59 @@
 #!/bin/bash
-echo "Please wait."
-wget -nc -q https://dl.google.com/dl/android/aosp/razor-ktu84p-factory-b1b2c0da.tgz
-tar zxf razor-ktu84p-factory-b1b2c0da.tgz
-cd razor-ktu84p
-unzip image-razor-ktu84p.zip
+echo "Please wait..."
+wget -nc -q https://dl.google.com/dl/android/aosp/razor-lrx21p-factory-ba55c6ab.tgz
+tar zxf razor-lrx21p-factory-ba55c6ab.tgz
+rm razor-lrx21p-factory-ba55c6ab.tgz
+cd razor-lrx21p
+unzip image-razor-lrx21p.zip
 cd ../
-./simg2img razor-ktu84p/system.img system.ext4.img
-mkdir system
+./simg2img razor-lrx21p/system.img system.ext4.img
 mkdir tmp
 sudo mount -o loop -t ext4 system.ext4.img tmp
 sync
+mkdir -p system/etc/preferred-apps
+mkdir -p system/etc/sysconfig
+mkdir -p system/etc/updatecmds
 mkdir -p system/lib/soundfx
 mkdir -p system/lib/hw
 mkdir -p system/vendor/etc
-mkdir -p system/vendor/firmware/discretix
 mkdir -p system/vendor/lib/drm
 mkdir -p system/vendor/lib/mediadrm
 mkdir -p system/vendor/media
-mkdir -p system/etc
 mkdir -p system/media
+mkdir -p system/media/audio/ringtones
+mkdir -p system/media/audio/ui
 
+cp -a tmp/etc/preferred-apps/google.xml system/etc/preferred-apps/google.xml
+cp -a tmp/etc/sysconfig/google.xml system/etc/sysconfig/google.xml
+cp -a tmp/etc/sysconfig/google_build.xml system/etc/sysconfig/google_build.xml
+cp -a tmp/etc/updatecmds/google_generic_update.txt system/etc/updatecmds/google_generic_update.txt
+cp -a tmp/etc/apns-conf.xml system/etc/apns-conf.xml
+cp -a tmp/etc/fmas_eq.dat system/etc/fmas_eq.dat
+cp -a tmp/lib/soundfx/libfmas.so system/lib/soundfx/libfmas.so
+cp -a tmp/lib/libDxHdcp.so system/lib/libDxHdcp.so
+cp -a tmp/lib/librefocus.so system/lib/librefocus.so
+cp -a tmp/lib/libvcdecoder_jni.so system/lib/libvcdecoder_jni.so
+cp -a tmp/lib/libvorbisencoder.so system/lib/libvorbisencoder.so
 cp -a tmp/vendor/etc/audio_effects.conf system/vendor/etc/audio_effects.conf
 cp -a tmp/vendor/lib/drm/libdrmwvmplugin.so system/vendor/lib/drm/libdrmwvmplugin.so
 cp -a tmp/vendor/lib/mediadrm/libwvdrmengine.so system/vendor/lib/mediadrm/libwvdrmengine.so
 cp -a tmp/vendor/lib/libWVStreamControlAPI_L1.so system/vendor/lib/libWVStreamControlAPI_L1.so
+cp -a tmp/vendor/lib/libWVphoneAPI.so system/vendor/lib/libWVphoneAPI.so
 cp -a tmp/vendor/lib/libfrsdk.so system/vendor/lib/libfrsdk.so
 cp -a tmp/vendor/lib/libwvdrm_L1.so system/vendor/lib/libwvdrm_L1.so
 cp -a tmp/vendor/lib/libwvm.so system/vendor/lib/libwvm.so
 cp -a tmp/vendor/media/LMspeed_508.emd system/vendor/media/LMspeed_508.emd
 cp -a tmp/vendor/media/PFFprec_600.emd system/vendor/media/PFFprec_600.emd
-cp -a tmp/etc/apns-conf.xml system/etc/apns-conf.xml
-cp -a tmp/etc/audio_effects.conf system/etc/audio_effects.conf
-cp -a tmp/etc/fmas_eq.dat system/etc/fmas_eq.dat
-cp -a tmp/lib/libDxHdcp.so system/lib/libDxHdcp.so
-cp -a tmp/lib/soundfx/libfmas.so system/lib/soundfx/libfmas.so
-cp -a tmp/lib/hw/keystore.msm8960.so system/lib/hw/keystore.msm8960.so
-cp -a tmp/lib/libgcastv2_base.so system/lib/libgcastv2_base.so
-cp -a tmp/lib/libgcastv2_support.so system/lib/libgcastv2_support.so
-cp -a tmp/lib/libjgcastservice.so system/lib/libjgcastservice.so
-cp -a tmp/lib/libjhead.so system/lib/libjhead.so
-cp -a tmp/lib/libjhead_jni.so system/lib/libjhead_jni.so
-cp -a tmp/media/bootanimation.zip system/media/bootanimation.zip
+cp -a tmp/media/audio/ui/NFCFailure.ogg system/media/audio/ui/NFCFailure.ogg
+cp -a tmp/media/audio/ui/NFCInitiated.ogg system/media/audio/ui/NFCInitiated.ogg
+cp -a tmp/media/audio/ui/NFCSuccess.ogg system/media/audio/ui/NFCSuccess.ogg
+cp -a tmp/media/audio/ui/NFCTransferComplete.ogg system/media/audio/ui/NFCTransferComplete.ogg
+cp -a tmp/media/audio/ui/NFCTransferInitiated.ogg system/media/audio/ui/NFCTransferInitiated.ogg
+cp -a tmp/media/audio/ui/VideoStop.ogg system/media/audio/ui/VideoStop.ogg
+cp -a tmp/media/audio/ui/audio_end.ogg system/media/audio/ui/audio_end.ogg
+cp -a tmp/media/audio/ui/audio_initiate.ogg system/media/audio/ui/audio_initiate.ogg
 
 sudo umount tmp
 rm -rf tmp
-rm -rf razor-ktu84p
+rm -rf razor-lrx21p
 rm system.ext4.img
